01ae5ee53e4ed1aefebb4674e5b0ac0ea161f249
alexander-merz <web.engineering.merz@gmail.com>
2021-08-11 21:55:30 +0200

    test usage of css custom properties

--- src/components/my-element.ts
+++ src/components/my-element.ts
@@ -12,8 +12,8 @@ export class MyElement extends LitElement {
   static styles = css`
     :host {
       display: block;
-      border: solid 1px gray;
-      padding: 16px;
+      border: var(--border, solid 1px gray);
+      padding: var(--padding, 16px);
       max-width: 800px;
     }
   `
